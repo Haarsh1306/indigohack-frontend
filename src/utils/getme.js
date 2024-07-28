90d8ad3deff241d00e330d76139105052350dcbb
Harsh Verma <hv41096@gmail.com>
2024-07-28 14:28:33 +0530

    getme

--- src/utils/getme.js
+++ src/utils/getme.js
@@ -1,17 +1,19 @@
 import axios from "axios";
 
 export const getme = async () => {
-    try {
-      const token = localStorage.getItem("token");
-      const result = await axios.get("http://localhost:3000/api/v1/getme", {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      if (result.data.message) {
-        return true;
-      } else return false;
-    } catch (error) {
-      return false;
-    }
-  };
+  try {
+    const token = localStorage.getItem("token");
+    if (!token) return false;
+
+    const result = await axios.get("http://localhost:3000/api/v1/getme", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return result.data.message ? true : false;
+  } catch (error) {
+    console.error("Error fetching /getme:", error);
+    return false;
+  }
+};
